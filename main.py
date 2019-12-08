f7b8002541bcb7d4ef0346949a7c7d22a19a93c7
Phelan Vendeville <phelan.vendeville@gmail.com>
2019-12-07 22:17:23 -0800

    Add custom message to landing page.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Ready to gird your loins with some runderwear?')
 
 
 app = webapp2.WSGIApplication([
