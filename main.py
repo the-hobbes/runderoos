f2f8b0491203eecea8275353c4ba8eda965ad96a
Phelan Vendeville <phelan.vendeville@gmail.com>
2019-12-08 16:37:27 -0800

    Initial stab at setting up layout.
    This includes the yaml file to identify the directories, as well as a landing
    page with some basic elements on it. Next steps will be to fix picture
    formatting, and figure out how to structure the page to fit the content.

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import os
+import logging
 import webapp2
+
+from google.appengine.ext.webapp import template
+
+
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
 
 class MainPage(webapp2.RequestHandler):
+
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Ready to gird your loins with some runderwear?')
+        path = TEMPLATE_DIR + '/index.html'
+        logging.info(path)
+        self.response.write(template.render(path, {}))
+
+class FeaturesPage(webapp2.RequestHandler):
+
+    def get(self):
+        path = TEMPLATE_DIR + '/features.html'
+        logging.info(path)
+        self.response.write(template.render(path, {}))
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/features', FeaturesPage),
 ], debug=True)
